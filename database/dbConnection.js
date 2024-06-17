05644bc2f1a1b4eb6dd9122cc018b1d883ae999c
Ahmed A. Eid <developtheparadox@gmail.com>
2024-06-17 21:37:39 +0300

    one commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,8 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-export const sequeli = new Sequelize('facebook_duplicate', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+export const sequeli = new Sequelize('mysql://ush9th0tuufmkcfd:T9nT9qg5Q6S5pg44wsDG@bsfyamhx90tfrfusdzu6-mysql.services.clever-cloud.com:3306/bsfyamhx90tfrfusdzu6')
 
 sequeli.authenticate().then(() => { console.log('SEQUELIZE CONNECTED SUCCESSFULLY TO DATABASE!'); }).catch(() => { console.log('SEQUELIZE FAILED TO CONNECT TO DATABASE!'); })
